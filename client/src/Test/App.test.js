d41ba144525f7909f90fcb01be4e2b5f5d432810
Alek <aleksebastian@gmail.com>
2021-10-27 22:25:26 -0500

    Corrected App file path in test file

--- client/src/Test/App.test.js
+++ client/src/Test/App.test.js
@@ -3,13 +3,13 @@
  */
 import "core-js/stable";
 import "regenerator-runtime/runtime";
-import React from 'react'
-import { render, screen } from '@testing-library/react'
-import '@testing-library/jest-dom'
-import App from '../components/App'
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import App from "../App";
 
-test('loads and displays greeting', async () => {
-  render(<App />)
+test("loads and displays greeting", async () => {
+  render(<App />);
 
-  expect(screen.getByText('Hello there!')).toBeInTheDocument();
+  expect(screen.getByText("Hello there!")).toBeInTheDocument();
 });
